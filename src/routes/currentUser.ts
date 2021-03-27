7ae1214438568e01d0dffb9c272dfc90907446c5
Joseph Reed <jereed559@gmail.com>
2021-03-27 03:42:46 -0700

    change post to get for currentuser route

--- src/routes/currentUser.ts
+++ src/routes/currentUser.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 const router = Router();
 import auth from '../middlewares/auth';
 
-router.post('/api/users/currentuser', auth, (req: Request, res: Response) => {
+router.get('/api/users/currentuser', auth, (req: Request, res: Response) => {
   res.send({msg: 'Made it to route', currentUser: req.body.cust_username })
 });
 
