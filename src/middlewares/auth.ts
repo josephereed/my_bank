bc48a5916397d9cae6f4c0554c2724693d374f24
Joseph Reed <jereed559@gmail.com>
2021-03-27 22:39:17 -0700

    ajax requests able to update redux state

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,18 +5,20 @@ require('dotenv').config();
 const auth = (req: Request, res: Response, next: Function) => {
   try {
     const token = req.headers['authorization'];
+    console.log('Token', token);
     if (!token) res.send({ msg: 'No token' }).status(400);
     const decoded = jwt.verify(token!, process.env.SECRET_KEY as string);
     if (decoded) {
       req.body.cust_username = decoded;
       next();
     } else {
-      res.send({ msg: decoded }).status(401);
+      res.status(401).send({ msg: decoded });
     }
   } catch (error) {
     res.status(401).send({ msg: 'Unauthorized', error: error.message });
   }
-  // Get token
+  // const token = req.headers['authorization']
+  // console.log('Token: ', token);
 };
 
 export default auth;
--- src/routes/currentUser.ts
+++ src/routes/currentUser.ts
@@ -3,7 +3,10 @@ const router = Router();
 import auth from '../middlewares/auth';
 
 router.get('/api/users/currentuser', auth, (req: Request, res: Response) => {
-  res.send({msg: 'Made it to route', currentUser: req.body.cust_username })
+  res.send({
+    currentUser: req.body.cust_username,
+    auth: req.body.auth,
+  });
 });
 
 export { router as currentUserRouter };
